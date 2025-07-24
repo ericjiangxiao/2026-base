79666d856936b429e1ae7ab5ea81177d1e533a0e
Eric Xiao <122230179+ericjiangxiao@users.noreply.github.com>
2025-07-24 09:06:10 -0400

    button controls updated

--- src/main.cpp
+++ src/main.cpp
@@ -34,12 +34,7 @@ void buttonL2_action() {
 
 void buttonR1_action() {
   chassis.stop(hold);
-  if (controller(primary).ButtonR2.pressing()) {  
-    score_middle();
-  }
-  else {
-    score_long();
-  }
+  score_long();
   // Wait until the button is released to stop the intake.
   while(controller(primary).ButtonR1.pressing()) {
     wait (20, msec);
@@ -51,10 +46,39 @@ void buttonR1_action() {
 // This function is called when the R2 button is pressed.
 void buttonR2_action() {
   chassis.stop(hold);
+  score_middle();
+  while(controller(primary).ButtonR2.pressing()) {
+    wait (20, msec);
+  }  
+  chassis.stop(coast);
+  stop_rollers();
+
+}
+
+// This function is called when the B button is pressed.
+// It holds the drivetrain in place until the button is released.
+void buttonB_action()
+{
+  double distance_traveled = (chassis.get_left_position_in() + chassis.get_right_position_in()) / 2.0;
+  chassis.stop(hold);
   controller(primary).rumble(".");
-  waitUntil(!controller(primary).ButtonR2.pressing());
+  wait(0.5, sec);
+  // Display the distance traveled on the controller screen.
+  controller(primary).Screen.print("distance: %.2f in.    ", distance_traveled);
+  waitUntil(!controller(primary).ButtonB.pressing());
   chassis.stop(coast);
 }
+
+// This function is called when the X button is pressed.
+void buttonX_action()
+{
+  // run macro function
+  chassis.driver_control_disabled = true;
+  // TODO: Insert macro code here. This is a placeholder for future macro actions triggered by Button X.
+
+  chassis.driver_control_disabled = false;
+}
+
 
 // Global flag auton_test_mode indicating whether the robot is in autonomous test mode.
 // When true, special button actions allow selection and testing of autonomous routines.
@@ -74,28 +98,31 @@ void buttonRight_action()
     show_auton_menu();
     return;
   }
-}
-
-// This function is called when the B button is pressed.
-// It holds the drivetrain in place until the button is released.
-void buttonB_action()
-{
-  chassis.stop(hold);
-  controller(primary).rumble(".");
-  waitUntil(!controller(primary).ButtonB.pressing());
-  chassis.stop(coast);
-}
-
-// This function is called when the X button is pressed.
-void buttonX_action()
-{
-  // run macro function
+  // otherwise, run ohter macro code
   chassis.driver_control_disabled = true;
-  // TODO: Insert macro code here. This is a placeholder for future macro actions triggered by Button X.
+  // TODO: Insert test code here. This is a placeholder for future actions triggered by Button Right.
+
+  chassis.driver_control_disabled = false;
+
+}
+
+// This function is called when the Left button is pressed.
+void buttonLeft_action()
+{
+  // if in test mode, scroll through the auton menu
+  if (auton_test_mode)
+  {
+    current_auton_selection = (current_auton_selection - 1) % auton_num;
+    show_auton_menu();
+    return;
+  }
+
+  // otherwise, run ohter macro code
+  chassis.driver_control_disabled = true;
+  // TODO: Insert test code here. This is a placeholder for future actions triggered by Button Left.
 
   chassis.driver_control_disabled = false;
 }
-
 
 // This function is called when the A button is pressed.
 void buttonA_action()
@@ -135,6 +162,7 @@ int main() {
 
   // Register the controller button callbacks.
   controller(primary).ButtonRight.pressed(buttonRight_action);
+  controller(primary).ButtonRight.pressed(buttonLeft_action);
   controller(primary).ButtonA.pressed(buttonA_action);
   controller(primary).ButtonB.pressed(buttonB_action);
   controller(primary).ButtonX.pressed(buttonX_action);
--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -8,15 +8,15 @@ using namespace vex;
 //                                Robot Configuration
 // ----------------------------------------------------------------------------
 
-// total number of motors, including drivetrain
-const int NUMBER_OF_MOTORS = 9;
-
 // motor and sensor definitions
 motor roller_bottom = motor(PORT11, ratio18_1, true);
 motor roller_middle = motor(PORT12, ratio18_1, true);
 motor roller_top = motor(PORT13, ratio6_1, true);
 
-void in_take_basket() {
+// total number of motors, including drivetrain
+const int NUMBER_OF_MOTORS = 9;
+
+void in_take() {
   roller_bottom.spin(forward, 12, volt);
   roller_middle.spin(forward, 12, volt);
   roller_top.stop(coast);
@@ -26,12 +26,6 @@ void out_take() {
   roller_bottom.spin(forward, -12, volt);
   roller_middle.spin(forward, -12, volt);
   roller_top.stop(coast);
-}
-
-void in_take() {
-  roller_bottom.spin(forward, 12, volt);
-  roller_middle.spin(forward, -12, volt);
-  roller_top.stop(hold);
 }
 
 void stop_rollers() {
